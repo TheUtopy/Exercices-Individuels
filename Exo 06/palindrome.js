1c8fdc9b55e5895c63a6760a051b2f46d8a1e224
TheUtopy <kevin.chron@gmail.com>
2023-03-15 16:17:02 +0100

    Fin exo sauf etape 4

--- Exo 06/palindrome.js
+++ Exo 06/palindrome.js
@@ -1,2 +1,73 @@
 // Etape 1
 
+function isValidDate(date) {
+    date = date.split("/")
+    if (!maxDaysInMonth(date[0], date[1]) || date[2] < 1000 || date[2] > 9999) {
+        return false
+    }
+    return true
+}
+
+function maxDaysInMonth(days, month) {
+    if (["01", "03", "05", "07", "08", "10", "12"].includes(month) && parseInt(days) <= 31) {
+        return true
+    }
+    else if (["04", "06", "09", "11"].includes(month) && parseInt(days) <= 30) {
+        return true
+    }
+    else if (month == 02 && parseInt(days) <= 29) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+// Etape 2
+
+// function isPalindrome(date) {
+//     date = date.replace("/", "")
+//     date = date.split("/")
+//     date[0] = date[0][3] + date[0][2] + date[0][1] + date[0][0]
+//     if (date[0] == date[1]) {
+//         return true
+//     }
+//     return false
+// }
+
+//Etape 3
+
+function getNextPalindromes(x) {
+    let date = "15/03/2023"
+    let result = []
+    let separated_date = date.split("/")
+    let truc
+    while (x > 0) {
+        if (isValidDate(separated_date.join("/"))) {
+            truc = separated_date[1][1] + separated_date[1][0] + separated_date[0][1] + separated_date[0][0]
+            if (separated_date[2] == truc) {
+                result.push(separated_date.join("/"))
+                x -= 1
+            }
+        }
+        if (parseInt(separated_date[2][3]) >= 2) {
+            if (separated_date[2][2] == "9") {
+                if (separated_date[2][1] == "9") {
+                    separated_date[2] = String(parseInt(separated_date[2][0]) + 1) + "000"
+                } else {
+                    separated_date[2][1] = separated_date[2][0] + String(parseInt(separated_date[2][1]) + 1) + "00"
+                }
+            } else {
+                separated_date[2] = separated_date[2][0] + separated_date[2][1] + String(parseInt(separated_date[2][2]) + 1) + "0"
+            }
+        } else {
+            separated_date[2] = separated_date[2][0] + separated_date[2][1] + separated_date[2][2] + String(parseInt(separated_date[2][3]) + 1)
+        }
+        separated_date[0] = separated_date[2][3] + separated_date[2][2]
+        separated_date[1] = separated_date[2][1] + separated_date[2][0]
+    }
+    return result
+}
+
+// Etape 04
+
